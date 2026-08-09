24818a959b3c0bb8cd27484688b5a4c3ea888c90
tripathy005 <satyaprakashtripathy05@gmail.com>
2026-08-09 11:50:18 +0530

    feat: Add @vitejs/plugin-basic-ssl for enhanced SSL support in Vite configuration

--- greenSquadApp/vite.config.js
+++ greenSquadApp/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
   server: {
-    host: 'localhost',  
-    port: 4000,         
-    open: false,       
+    host: true,
+    port: 7000,
+    open: false,
   },
 })
